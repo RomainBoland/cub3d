08378a2c4084d5ec277916bc674e2a7bdef81cb6
root <root@jjiro.localdomain>
2025-07-17 15:09:25 +0200

    Added Map sctruct

--- includes/cub3d.h
+++ includes/cub3d.h
@@ -13,17 +13,35 @@
 #ifndef CUB3D_H
 # define CUB3D_H
 
+/* ------------ */
+/*	INCLUDES	*/
+/* ------------	*/
+
 # include "../libft/includes/libft.h"
 # include "../mlx/mlx.h"
 # include <fcntl.h>
 # include <math.h>
 # include <stdio.h>
 
-/* ---------- */
-/* STRUCTURES */
-/* ---------- */
+/* ------------	*/
+/* 	STRUCTURES 	*/
+/* ------------	*/
 
-/* ------------ */
+typedef struct s_config
+{
+    char    *north_texture;
+    char    *south_texture;
+    char    *west_texture;
+    char    *east_texture;
+    int     floor_color[3];     // RGB
+    int     ceiling_color[3];   // RGB
+    char    **map;
+    int     map_width;
+    int     map_height;
+    // player start position and orientation
+}   t_config;
+
+/* ------------	*/
 /* 	FUNCTIONS	*/
 /* ------------ */
 
@@ -31,13 +49,11 @@
 int		main(int argc, char **argv);
 
 /*	ERROR	*/
-
 // print_error.c
 void	print_error(const char *message);
 int		exit_error(const char *message);
 
 /*	PARSING	*/
-
 // parse_file.c
 int		open_map(const char *file_path);
 int		str_ends_with(const char *str, const char *suffix);
